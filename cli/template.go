753d4eddb2829d0cd5dc8e04b896fb62310f2d63
Marcin Ptaszyński <marcin.ptaszynski@ntti3.com>
2017-04-28 16:12:46 -0700

    mark sql property as extension to openapi

--- cli/template.go
+++ cli/template.go
@@ -22,7 +22,7 @@ import (
 func deleteGohanExtendedProperties(node map[string]interface{}) {
 	extendedProperties := [...]string{"unique", "permission", "relation",
 		"relation_property", "view", "detail_view", "propertiesOrder",
-		"on_delete_cascade", "indexed", "relationColumn"}
+		"on_delete_cascade", "indexed", "relationColumn", "sql"}
 
 	for _, extendedProperty := range extendedProperties {
 		delete(node, extendedProperty)
