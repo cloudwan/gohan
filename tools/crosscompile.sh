6e50f410c584f6eade6bb90bea5f3bf816b020d9
Iwasaki Yudai <yudai.iwasaki@ntti3.com>
2017-05-17 17:00:38 -0700

    Remove 386 from cross compile targets

--- tools/crosscompile.sh
+++ tools/crosscompile.sh
@@ -3,7 +3,7 @@
 mkdir dist
 rm -rf dist/*
 cd dist
-xgo --targets=windows/amd64,windows/386,darwin/386,darwin/amd64,linux/amd64 github.com/cloudwan/gohan
+xgo --targets=windows/amd64,darwin/amd64,linux/amd64 github.com/cloudwan/gohan
 for binary in $(ls); do
     zip -r ${binary}.zip ${binary}
     rm -rf ${binary}
