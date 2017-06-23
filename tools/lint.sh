3664922f74ad04b89d3c10c344f4ec9a0d0d247c
Przemyslaw Dobrowolski <przemyslaw.dobrowolski@ntti3.com>
2017-06-23 13:11:00 +0200

    Ignore linter suggestiions for generated mock files

--- tools/lint.sh
+++ tools/lint.sh
@@ -2,7 +2,7 @@
 
 #set -e
 
-for file in `find . -name "*.go" | grep -v vendor | grep -v go-bindata.go | grep -v op.go`; do
+for file in `find . -name "*.go" | grep -v vendor | grep -v bindata.go |grep -v mocks | grep -v op.go`; do
   golint $file
   go vet $file
   misspell -error $file
