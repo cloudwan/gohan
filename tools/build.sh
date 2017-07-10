0681c733610eed2069ef12c7c92e5e1d5b81ca87
Przemyslaw Dobrowolski <przemyslaw.dobrowolski@ntti3.com>
2017-07-10 08:45:19 -0700

    Temp: remove build version flag

--- tools/build.sh
+++ tools/build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 # go run ./extension/gohanscript/tools/gen.go genlib -t extension/gohanscript/templates/lib.tmpl -p github.com/cloudwan/gohan/extension/gohanscript/lib -e autogen -ep extension/gohanscript/autogen
-go build -ldflags "-X main.BuildVersion=`git rev-parse HEAD`" main.go
-
+#go build -ldflags "-X main.BuildVersion=`git rev-parse HEAD`" main.go
+go build
