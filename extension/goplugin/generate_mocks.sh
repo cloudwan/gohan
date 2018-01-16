65c90eff14eedf8c9e063439fe2c566a7f971a67
jkmar <marciniszyn.jk@gmail.com>
2018-01-16 15:58:06 +0100

    fix generate mocks

--- extension/goplugin/generate_mocks.sh
+++ extension/goplugin/generate_mocks.sh
@@ -2,5 +2,5 @@
 
 file="environment_mock_gen.go"
 mockgen -package goext github.com/cloudwan/gohan/extension/goext ICore,ILogger,ISchemas,ISync,IDatabase,ITransaction,IHTTP,IAuth,IConfig,IUtil > ${file}
-sed -i 's/goext\.//g' ${file}
+sed -i '/goext"$/d;s/goext\.//g' ${file}
 mv ${file} ../goext/
