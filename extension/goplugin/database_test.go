39d8e99eb7142b71f6cf4060e87fb214525a43b6
Bartosz Kąkol <bartosz@bartosz-kakol.intra.codilime.com>
2018-07-03 17:03:30 +0200

    BL-208 Transaction Context Timeout Fix

--- extension/goplugin/database_test.go
+++ extension/goplugin/database_test.go
@@ -16,10 +16,13 @@
 package goplugin_test
 
 import (
+	"context"
 	"fmt"
+	"time"
 
 	"github.com/cloudwan/gohan/db/mocks"
 	"github.com/cloudwan/gohan/db/options"
+	"github.com/cloudwan/gohan/db/transaction"
 	"github.com/cloudwan/gohan/db/transaction/mocks"
 	"github.com/cloudwan/gohan/extension/goext"
 	"github.com/cloudwan/gohan/extension/goplugin"
@@ -83,6 +86,20 @@ var _ = Describe("Database", func() {
 				Expect(err).To(HaveOccurred())
 				Expect(tx).To(BeNil())
 			})
+
+			It("should call inner function using Context with Timeout", func() {
+				tempCtxWithTimeout, _ := context.WithTimeout(context.Background(), 50*time.Millisecond)
+				ctx := goext.Context{
+					"context": tempCtxWithTimeout,
+				}
+				mockDB.EXPECT().BeginTx(gomock.Any(), gomock.Any()).Return(nil, nil).
+					Do(func(ctx context.Context, options *transaction.TxOptions) {
+						_, hasDeadline := ctx.Deadline()
+						Expect(hasDeadline).To(BeTrue())
+					})
+
+				db.BeginTx(ctx, &goext.TxOptions{})
+			})
 		})
 
 		Context("Panic in transaction", func() {
@@ -106,8 +123,8 @@ var _ = Describe("Database", func() {
 
 				env := goplugin.NewMockIEnvironment(nil, nil)
 				env.SetMockModules(goext.MockModules{
-					Util:     true,
-					Database: true,
+					Util:            true,
+					Database:        true,
 					DefaultDatabase: true,
 				})
 				env.MockUtil().EXPECT().GetTransaction(gomock.Any()).Return(
