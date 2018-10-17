d899c3fc28fa01f54826dd34670b4be1b5512b54
Maciej Zimnoch <mzimnoch@hotmail.com>
2018-10-17 15:20:59 -0700

    Change golint import path (#690)
    Because of recent changes in golint repository,
    it's enforced that golint package must be imported from
    golang.org/x/lint/golint. This commit changes dev_setup script to pull
    proper version

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 go get -u github.com/kardianos/govendor
-go get -u github.com/golang/lint/golint
+go get -u golang.org/x/lint/golint
 go get -u github.com/gordonklaus/ineffassign
 go get -u github.com/jteeuwen/go-bindata/go-bindata
 go get -u github.com/client9/misspell/cmd/misspell
