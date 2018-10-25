f687bdb140d24778b193b43da4af15f80bdc5a1a
Maciej Zimnoch <maciej.zimnoch@ntti3.io>
2018-10-25 16:26:54 +0200

    Replace mockery with gomock used in Gohan Mocks

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -5,4 +5,4 @@ go get -u golang.org/x/lint/golint
 go get -u github.com/gordonklaus/ineffassign
 go get -u github.com/jteeuwen/go-bindata/go-bindata
 go get -u github.com/client9/misspell/cmd/misspell
-go get -u github.com/vektra/mockery/cmd/...
+go get -u github.com/golang/mock/gomock
