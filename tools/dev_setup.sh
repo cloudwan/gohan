41f5afae85cab02d79e5602252b192f396405495
Adrian Stepinski <adrian.stepinski@ntti3.io>
2018-12-18 16:24:50 +0100

    Fixed go-bindata

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -3,6 +3,6 @@
 go get -u github.com/kardianos/govendor
 go get -u golang.org/x/lint/golint
 go get -u github.com/gordonklaus/ineffassign
-go get -u github.com/jteeuwen/go-bindata/go-bindata
+go get -u github.com/kevinburke/go-bindata/go-bindata
 go get -u github.com/client9/misspell/cmd/misspell
 go get -u github.com/golang/mock/gomock
