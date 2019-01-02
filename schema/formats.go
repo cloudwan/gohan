7acf6f75609ed34cd56d7a016b40eb32ff6d1766
Jakub Tompolski <jakub.tompolski@ntti3.io>
2019-01-02 18:36:24 +0100

    CW-997 Ip format validate 00 and 000

--- schema/formats.go
+++ schema/formats.go
@@ -27,6 +27,7 @@ import (
 )
 
 type macFormatChecker struct{}
+type ipv4FormatChecker struct{}
 type cidrFormatChecker struct{}
 type cidrOrIPv4FormatChecker struct{}
 type ipv4NetworkFormatChecker struct{}
@@ -41,25 +42,57 @@ type textFormatChecker struct{}
 type versionFormatChecker struct{}
 type versionConstraintFormatChecker struct{}
 
+var yangIPv4FormatRegexp = regexp.MustCompile(`^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(%[\p{N}\p{L}]+)?`)
+
 func (f macFormatChecker) IsFormat(input string) bool {
 	match, _ := regexp.MatchString(`^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$`, input)
 	return match
 }
 
+func matchYangIPv4AddressPattern(input string) bool {
+	return yangIPv4FormatRegexp.MatchString(input)
+}
+
+func isValidIP(ip net.IP) bool {
+	return ip != nil
+}
+
+func isV4(ip net.IP) bool {
+	return ip.To4() != nil
+}
+
+func (f ipv4FormatChecker) IsFormat(input string) bool {
+	ip := net.ParseIP(input)
+	if !isValidIP(ip) || !isV4(ip) {
+		return false
+	}
+	return matchYangIPv4AddressPattern(input)
+}
+
 func (f cidrFormatChecker) IsFormat(input string) bool {
-	_, _, err := net.ParseCIDR(input)
-	return err == nil
+	ip, _, err := net.ParseCIDR(input)
+	if err != nil {
+		return false
+	}
+	if isV4(ip) {
+		return matchYangIPv4AddressPattern(input)
+	}
+	return true
 }
 
 func (f cidrOrIPv4FormatChecker) IsFormat(input string) bool {
-	cidrIP, _, cidrErr := net.ParseCIDR(input)
-	ip := net.ParseIP(input)
-	return (cidrErr == nil && cidrIP.To4() != nil) || (ip != nil && ip.To4() != nil)
+	ipv4 := ipv4FormatChecker{}
+	cidr := ipv4AddressWithCidrFormatChecker{}
+	return ipv4.IsFormat(input) || cidr.IsFormat(input)
 }
 
 func (f ipv4NetworkFormatChecker) IsFormat(input string) bool {
 	hostIP, netIP, _, cidrErr := extractHostAndNet(input)
 	if cidrErr != nil {
+		return false
+	}
+
+	if !matchYangIPv4AddressPattern(input) {
 		return false
 	}
 	return hostIP.Equal(netIP)
@@ -72,6 +105,10 @@ func (f ipv4AddressWithCidrFormatChecker) IsFormat(input string) bool {
 	}
 
 	if isBroadcast(hostIP, mask) {
+		return false
+	}
+
+	if !matchYangIPv4AddressPattern(input) {
 		return false
 	}
 	return !hostIP.Equal(netIP)
@@ -145,6 +182,7 @@ func (f versionConstraintFormatChecker) IsFormat(input string) bool {
 
 func registerGohanFormats(checkers gojsonschema.FormatCheckerChain) {
 	checkers.Add("mac", macFormatChecker{})
+	checkers.Add("ipv4", ipv4FormatChecker{})
 	checkers.Add("cidr", cidrFormatChecker{})
 	checkers.Add("cidr-or-ipv4", cidrOrIPv4FormatChecker{})
 	checkers.Add("ipv4-network", ipv4NetworkFormatChecker{})
