23299ae650fcc06f0eb59c2626f8ce3ee18f2f7c
Alec Chepkin <alec.chepkin@ntti3.io>
2023-05-04 12:49:25 +0100

    new docker image, fix deps

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go get -u github.com/kardianos/govendor
-go get -u golang.org/x/lint/golint
-go get -u github.com/gordonklaus/ineffassign
-go get -u github.com/kevinburke/go-bindata/go-bindata
-go get -u github.com/client9/misspell/cmd/misspell
-go get -u github.com/golang/mock/gomock
+go install github.com/kardianos/govendor
+go install golang.org/x/lint/golint
+go install github.com/gordonklaus/ineffassign
+go install github.com/kevinburke/go-bindata/...
+go install github.com/client9/misspell/cmd/misspell
+go install github.com/golang/mock/mockgen
