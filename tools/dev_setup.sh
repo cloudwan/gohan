545a20a18194920d128f2cd4fca948c2399b9343
Alec Chepkin <alec.chepkin@ntti3.io>
2023-05-05 15:07:19 +0100

    go-bindata fix4

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -3,6 +3,6 @@
 go install github.com/kardianos/govendor@latest
 go install golang.org/x/lint/golint@latest
 go install github.com/gordonklaus/ineffassign@latest
-go install github.com/kevinburke/go-bindata@v1.1.0
+go install github.com/kevinburke/go-bindata@v3.2.0
 go install github.com/client9/misspell/cmd/misspell@latest
 go install github.com/golang/mock/mockgen@latest
