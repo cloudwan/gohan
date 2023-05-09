4a62151f6f681e3481fbddab433436cc752b2f41
Alec Chepkin <alec.chepkin@ntti3.io>
2023-05-09 16:36:43 +0100

    add versions to dev_setup

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 go install github.com/kardianos/govendor@v1.0.9
-go install golang.org/x/lint/golint@latest
+go install golang.org/x/lint/golint@0.0.0-20210508222113-6edffad5e616
 go install github.com/gordonklaus/ineffassign@v0.0.0-20230107090616-13ace0543b28
 go install github.com/kevinburke/go-bindata/go-bindata@v3.2.0
-go install github.com/client9/misspell/cmd/misspell@latest
-go install github.com/golang/mock/mockgen@latest
+go install github.com/client9/misspell/cmd/misspell@v0.3.4
+go install github.com/golang/mock/mockgen@v1.6.0
