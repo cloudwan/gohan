ea64ae6e2c8d52ac5b11849dc82d7250eb75e4d3
Alec Chepkin <alec.chepkin@ntti3.io>
2023-05-09 16:03:38 +0100

    dev_setup ineffassign,govendor

--- tools/dev_setup.sh
+++ tools/dev_setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-go install github.com/kardianos/govendor@latest
+go install github.com/kardianos/govendor@v1.0.9
 go install golang.org/x/lint/golint@latest
-go install github.com/gordonklaus/ineffassign@latest
+go install github.com/gordonklaus/ineffassign@v0.0.0-20230107090616-13ace0543b28
 go install github.com/kevinburke/go-bindata/go-bindata@v3.2.0
 go install github.com/client9/misspell/cmd/misspell@latest
 go install github.com/golang/mock/mockgen@latest
